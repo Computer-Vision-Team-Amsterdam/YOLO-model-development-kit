3b01a3af5470e9eb2f01a1914f02b2aa4820ba17
Daan Bloembergen <daan.bloembergen@gmail.com>
2024-10-31 13:39:29 +0100

    Move aml_interface to root __init__.py

--- yolo_model_development_kit/__init__.py
+++ yolo_model_development_kit/__init__.py
@@ -11,3 +11,5 @@ config_path = os.path.abspath(
 )
 YoloModelDevelopmentKitSettings.set_from_yaml(config_path)
 settings = YoloModelDevelopmentKitSettings.get_settings()
+
+__all__ = [aml_interface, settings]
